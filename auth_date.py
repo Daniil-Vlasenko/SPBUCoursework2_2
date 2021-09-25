7c51879c18c748ca19e19db5cea625c88e156c40
Daniil-Vlasenko <74912458+Daniil-Vlasenko@users.noreply.github.com>
2021-09-25 09:02:37 +0300

    add password and username

--- auth_date.py
+++ auth_date.py
@@ -1,12 +1,8 @@
-# username = "viiisualart"
-# password = "k3k8Gm44"
 username = "contrarto"
 password = "P16rii8"
-# nameBase = "arts_dataset_test"
-nameBase = "arts_dataset"
-passwordBase = "333555111"
-postPath = r"C:\Users\Daniil\PycharmProjects\InstagramBot\web\post"
-webdriverPath = r"C:\Users\Daniil\PycharmProjects\InstagramBot\chromedriver\chromedriver.exe"
+
+postPath = 
+webdriverPath =
 
 
 accountsForPromotion = ['https://www.instagram.com/europe_treasures/?hl=ru', 'https://www.instagram.com/themuseumofmodernart/?hl=ru', 'https://www.instagram.com/medieval_suffering/?hl=ru', 'https://www.instagram.com/helloartists/?hl=ru', 'https://www.instagram.com/artbasel/?hl=ru', 'https://www.instagram.com/nilufargallery/?hl=ru', 'https://www.instagram.com/gagosian/?hl=ru', 'https://www.instagram.com/saatchiart/?hl=ru', 'https://www.instagram.com/tretyakov_gallery/?hl=ru', 'https://www.instagram.com/erarta_museum/?hl=ru']
